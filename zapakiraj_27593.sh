3a70625ba9f289d92a80733519b49b6b0df7bb99
vboxuser <vboxuser@Runnerji.myguest.virtualbox.org>
2024-04-14 10:28:28 +0200

    asdsa

--- zapakiraj_27593.sh
+++ zapakiraj_27593.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 # Build Docker image
 docker build . --file Dockerfile --tag najks/orv_vaja2:latest
 
