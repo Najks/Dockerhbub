9cf48fef2b2c55b2f032d9a2d202f4efc3953fdd
vboxuser <vboxuser@Runnerji.myguest.virtualbox.org>
2024-04-14 10:54:04 +0200

    zadni att

--- zapakiraj_27593.sh
+++ zapakiraj_27593.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 # Build Docker image
 docker build . --file Dockerfile --tag najks/orv_vaja2:latest
+
+# Log into DockerHub using GitHub secrets
+echo ${{ secrets.DOCKER_PASSWORD }} | docker login --username ${{ secrets.DOCKER_USERNAME }} --password-stdin
 
 # Push Docker image to DockerHub
 docker push najks/orv_vaja2:latest
